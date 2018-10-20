c2fb695b41e253b4ebd83f963f1134adb2d8b31a
C.Bauer <chris@codecommerce.de>
2018-10-20 02:06:24 +0200

    adding logg

--- source/index.php
+++ source/index.php
@@ -8,6 +8,10 @@ if ($bTest) {
     $json = json_decode(file_get_contents(__DIR__ . "/RequestTests/NextSessionRequest.json"));
 } else {
     $json = json_decode(file_get_contents('php://input'));
+    $logger = new Monolog\Logger('requestlog');
+    $f = fopen("../log/request_" . date("His_dmY") . ".log", "a+");
+    $logger->pushHandler(new StreamHandler($f, $logger::WARNING));
+    $logger->warning(file_get_contents('php://input'));
 }
 
-$alexaApi = new CodeCommerce\AlexaApi\Controller\RequestHandler($json);
+$alexaApi = new CodeCommerce\AlexaApi\Controller\RequestHandler($json, $logger);
