f75ebb97eae757e5df811d07f23b448b87c7b175
C.Bauer <chris@codecommerce.de>
2018-10-20 02:11:28 +0200

    Testing to get this skill running ...

--- source/index.php
+++ source/index.php
@@ -1,15 +1,18 @@
 <?php
 
+use Monolog\Handler\StreamHandler;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $bTest = false;
+
+$logger = new Monolog\Logger('requestlog');
 
 if ($bTest) {
     $json = json_decode(file_get_contents(__DIR__ . "/RequestTests/NextSessionRequest.json"));
 } else {
     $json = json_decode(file_get_contents('php://input'));
-    $logger = new Monolog\Logger('requestlog');
-    $f = fopen("../log/request_" . date("His_dmY") . ".log", "a+");
+    $f = fopen(__DIR__ . "/../log/request_" . date("His_dmY") . ".log", "a+");
     $logger->pushHandler(new StreamHandler($f, $logger::WARNING));
     $logger->warning(file_get_contents('php://input'));
 }
