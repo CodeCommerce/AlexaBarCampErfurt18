fcf1bc1fc7bb5e231d237d44baea27c64fc321bd
C.Bauer <chris@codecommerce.de>
2018-11-09 15:32:45 +0100

    testing background image

--- Alexa/Intents/SubmittedSessionsIntent.php
+++ Alexa/Intents/SubmittedSessionsIntent.php
@@ -63,15 +63,16 @@ class SubmittedSessionsIntent implements IntentsInterface
         $response = new Response($outSpeech);
 
         if ($viewPort = $this->system->getViewport()) {
-            $template = new Template(Template::BODY_TEMPLATE_2_IMAGE_LIMITED_CENTERED_TEXT);
-            $template->setTitle('Hallo')
-                ->setBackButton($template::BACK_BUTTON_VISIBLE)
-                ->setPrimary('Text 1');
+//            $template = new Template(Template::BODY_TEMPLATE_2_IMAGE_LIMITED_CENTERED_TEXT);
+//            $template->setTitle('Hallo')
+//                ->setBackButton($template::BACK_BUTTON_VISIBLE)
+//                ->setPrimary('Text 1');
             if ($viewPort->isDevice($viewPort::DEVICE_TYPE_SPOT)) {
-                $template->addBackgroundImage('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/FuBK_testcard_vectorized.svg/1536px-FuBK_testcard_vectorized.svg.png', 'test');
+                $response->setDirectives($this->getTestDirective());
+//                $template->addBackgroundImage('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/FuBK_testcard_vectorized.svg/1536px-FuBK_testcard_vectorized.svg.png', 'test');
             }
-            $directives = new Directives($template);
-            $response->setDirectives($directives);
+//            $directives = new Directives($template);
+//            $response->setDirectives($directives);
         }
 
         $responseBody = new ResponseBody($response);
